192a2d99bae036220ad336b55d1d827f2cba23ab
kugyousha <johnovalle@gmail.com>
2014-10-15 22:12:59 -0400

    add will_paginate gem and paginate links on index

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '4.0.7'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
+gem 'will_paginate', '~> 3.0'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def index
-    @links = Link.all
+    params[:per_page] ||= 1
+    params[:page]     ||= 1
+    @links = Link.paginate(page: params[:page], per_page: params[:per_page])
+    
   end
 end
