806c96df3ef8c242b25a88186cf6e9fce7cda60c
kugyousha <johnovalle@gmail.com>
2014-10-28 19:35:45 -0400

    add votes model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :links
   has_many :comments
+  has_many :votes
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017034831) do
+ActiveRecord::Schema.define(version: 20141028232859) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -47,5 +47,11 @@ ActiveRecord::Schema.define(version: 20141017034831) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: true do |t|
+    t.integer "user_id"
+    t.integer "link_id"
+    t.boolean "up"
+  end
 
 end
