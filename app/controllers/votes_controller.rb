328649d0784f9b5cd4af670a7fe61d8f37b654cd
kugyousha <johnovalle@gmail.com>
2014-10-28 20:17:20 -0400

    restrict voting to one vote per link per user

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,8 @@
 class VotesController < ApplicationController
   before_filter :authenticate_user!, only: [:create]
   def create
-    @vote = current_user.votes.create(vote_params)
+    @vote = current_user.votes.where(link_id: vote_params["link_id"].to_i).first || current_user.votes.create(vote_params)
+    @vote.update_attributes(up: vote_params["up"])
     redirect_to :back
   end
   
